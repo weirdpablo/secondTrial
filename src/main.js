087528015a0017c13c69ab3e32060b535f7112fd
Oyè! <55864374+weirdpablo@users.noreply.github.com>
2023-01-04 19:38:33 +0100

    update main.js
    This is just the fixed version

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
-
-console.log('this is what we do')
-
-createBadge()
-animateTitle()
+alert('local')
