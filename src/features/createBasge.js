3a465631fe7dc62f8d28c9c1bcd5956c5d56b48f
Oyè! <55864374+weirdpablo@users.noreply.github.com>
2023-01-04 19:09:37 +0100

    First commit main.js
    Just testing things out

--- src/features/createBasge.js
+++ src/features/createBasge.js
@@ -2,7 +2,7 @@ function createBadge() {
   // Create the badge
   const badge = document.createElement('div')
   badge.classList.add('js-badge')
-  badge.innerText = 'It works!'
+  badge.innerText = 'Hopefully we dont screw this up again!'
 
   // Add click envent to remove the badge from the DOM
   badge.addEventListener('click', (event) => {
