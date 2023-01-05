02619a6c8abe957ba83a41acd17c12f14921b5b1
Oyè! <55864374+weirdpablo@users.noreply.github.com>
2023-01-05 21:51:18 +0100

    update main.js
    reformatted to the required specs

--- src/main.js
+++ src/main.js
@@ -1,126 +1,60 @@
 console.log('This is a new message to test the next push')
-
 const texts = [
-  'Birds are fascinating creatures with a wide variety of shapes, sizes, and behaviors. From the tiny hummingbird to the majestic eagle, there is a bird to suit every taste. Some birds are known for their beautiful songs, while others are prized for their colorful feathers. Many species of birds are also important indicators of the health of the environment, as they are sensitive to changes in their habitat and can provide valuable insights into the overall health of an ecosystem. Whether youre a seasoned birder or just starting to appreciate these feathered friends, there is much to discover and admire about the world of birds',
+  'Quantum physics is a branch of physics that deals with the behavior of matter and energy at the atomic and subatomic scale.\n\n' +
+    'It is based on the idea that particles, such as atoms and photons, can exhibit both wave-like and particle-like properties, and that their behavior is fundamentally probabilistic rather than deterministic.\n\n' +
+    'Imagine the illustration is what happens within the atom. It shows the wave-particle duality properties of subatomic particles. It would be difficult to accurately determine the position of the particles because they’re  constantly being jostled by the noise.\n\n' +
+    'Similarly, in quantum physics, it is difficult to accurately measure the position and momentum of a particle because it is constantly being influenced by the unpredictable fluctuations of the quantum field.\n\n' +
+    'Just as the noise in the room makes it difficult to determine the position of an object, the uncertainty principle in quantum physics states that it is fundamentally impossible to precisely determine both the position and momentum of a particle at the same time.\n\n' +
+    'This uncertainty is not due to a lack of precision in our measuring instruments, but rather a fundamental feature of the quantum world.',
+
+  'Color theory is a set of principles used to determine which colors will look appealing when used together. It is based on the way that the human eye perceives color and how different colors interact with each other.\n\n' +
+    'Imagine the color box in the illustration. It is a gradient. A combination of primary, secondary and tertiary colors. There are three primary colors: red, yellow, and blue. These colors cannot be created by mixing other colors and are the basis for all other colors.\n\n' +
+    'Secondary colors are created by mixing two primary colors. The secondary colors are orange (made by mixing red and yellow), green (made by mixing blue and yellow), and purple (made by mixing blue and red).\n\n' +
+    'Tertiary colors are created by mixing a primary color with a secondary color. There are six tertiary colors: yellow-green, blue-green, blue-purple, red-purple, red-orange, and yellow-orange.',
+
+  'Yes, I can perform basic arithmetic calculations such as addition, subtraction, multiplication, and division. I can also solve more complex math problems and provide explanations and step-by-step solutions if needed.\n\n' +
+    'For example, if you give me the following math problem:\n\n' +
+    'Solve for x: 7x + 1 = 8\n\n' +
+    'I can solve it as follows:\n\n' +
+    "'3x + 2 = 8' \n" +
+    "'-1 -1' \n" +
+    " '7x = 7' \n" +
+    " 'x = 1' \n\n" +
+    'So the solution to this problem is x = 1.\n\n' +
+    "I can also handle equations with variables on both sides, inequalities, and systems of equations. Just let me know what you need help with and I'll do my best to assist you.",
+
+  "Yes, I'd be happy to explain what a torus is. I’ll using doughnuts as an analogy.\n\n" +
+    'A torus is a three-dimensional shape that looks like a doughnut. It is formed by rotating a circle around an axis that is perpendicular to the plane of the circle.\n\n' +
+    'Imagine taking a circular piece of dough and wrapping it around a tube so that the ends meet, forming a doughnut shape. This shape is similar to a torus, with the tube representing the axis around which the circle is rotated.\n\n' +
+    'A torus has a hole in the center, similar to a doughnut, and has a circular cross-section when viewed from the top or bottom. The distance from the center of the torus to the center of the circular cross-section is known as the major radius, and the radius of the circular cross-section is known as the minor radius.',
+
+  'Certainly! Here is an expression you can use to calculate the volume of a torus with a major radius R = 10 and a minor radius r = 5:\n\n' +
+    'V = (2 * pi^2 * R * r^2)\n\n' +
+    'Plugging in the given values of R and r, we get:\n\n' +
+    'V = (2 * pi^2 * 10 * 5^2) = (2 * pi^2 * 10 * 25) = 2 * pi^2 * 250 = 500 * pi^2 \n\n' +
+    'So, the volume of this torus would be approximately 500 * pi^2.',
+
+  'Not a lot. I’ve only been trained in physics and a bit of math. But I can try answering a few simple questions.\n\n' +
+    'Here’s a what Biology is using a mushroom as analogy;\n\n' +
+    'Biology is the scientific study of living organisms and their interactions with each other and their environment. This includes the study of their structure, function, growth, evolution, and distribution.\n\n' +
+    'Like all living things, mushrooms have a life cycle that includes growth, reproduction, and death. Mushrooms start as spores, which are tiny, seed-like structures that are produced by mature mushrooms. Spores can be carried by the wind or by animals and can grow into new mushrooms when they find a suitable environment.\n\n' +
+    'I also tried to make an illustration for you using mushrooms.',
+
+  'Hmm… now that’s a tough one. I apologize, I currently have insufficient key value pairs for certain concepts in Neuroscience. While I do understand what a tumor is, I don’t know what Gangliocytomas are yet.\n\n' +
+    'But here’s what I know about tumors;\n\n' +
+    'A tumor is an abnormal growth of cells in the body. Tumors can be benign, meaning they are not cancerous and do not spread to other parts of the body, or malignant, meaning they are cancerous and can spread to other parts of the body.\n\n' +
+    'Benign tumors are usually not a serious health threat, although they may cause problems if they press on vital organs or blood vessels. Malignant tumors, on the other hand, can be more serious because they can spread to other parts of the body and disrupt the normal function of organs and tissues.\n\n' +
+    'Tumors can occur in any part of the body and can be caused by a variety of factors, including genetic mutations, exposure to toxins or radiation, and certain infections or diseases.\n\n' +
+    'I also tried to make an illustration for you.',
+
+  'Certainly! Here’s a game.',
+
+  'I apologize if the game I rendered didn’t meet your expectations. I currently cannot render a game with sophisticated logic. I’ve only been trained with qualitative physics so far.',
+
+  'No problem!\n' +
+    'I’m glad I was able to help. Please feel free to ask me if you need to learn anything.\n' +
+    'Till next time.\n' +
+    'Clearing conversations and shutting down.',
 ]
-const speeds = [50, 65, 80]
-const dots1 = ['.', '..', '...']
-let r = 0
-let s = 0
-let currentAnimation = 0
-let revealFunctions = [revealDivs, revealDivs2, revealDivs3]
-let currentFunction = 0
-let previousFunction = revealFunctions.length - 1
-const autoScroll = document.getElementById('autoScroll')
-function getInput() {
-  let input = document.getElementById('input-f').value
-  if (input) {
-    document.getElementById('answer').innerHTML = input
-    document.getElementById('input-f').value = ''
-  }
-}
-function revealDivs() {
-  document.getElementById('container1').style.display = 'flex'
 
-  document.getElementById('container' + (previousFunction + 1)).style.display =
-    'none'
-
-  setTimeout(function () {
-    document.getElementById('div1').style.display = 'block'
-  }, 500)
-
-  setTimeout(function () {
-    document.getElementById('div1').style.display = 'none'
-  }, 6000)
-
-  setTimeout(function () {
-    document.getElementById('div2').style.display = 'block'
-  }, 6000)
-}
-function revealDivs2() {
-  document.getElementById('container2').style.display = 'flex'
-  document.getElementById('container' + (previousFunction + 1)).style.display =
-    'none'
-  setTimeout(function () {
-    document.getElementById('div3').style.display = 'block'
-  }, 1000)
-  setTimeout(function () {
-    document.getElementById('div3').style.display = 'none'
-  }, 9000)
-  setTimeout(function () {
-    document.getElementById('div4').style.display = 'block'
-  }, 9000)
-}
-function revealDivs3() {
-  document.getElementById('container3').style.display = 'flex'
-  document.getElementById('container' + (previousFunction + 1)).style.display =
-    'none'
-  setTimeout(function () {
-    document.getElementById('div5').style.display = 'block'
-  }, 1000)
-  setTimeout(function () {
-    document.getElementById('div5').style.display = 'none'
-  }, 10000)
-  setTimeout(function () {
-    document.getElementById('div6').style.display = 'block'
-  }, 10000)
-}
-function updateDots1() {
-  document.getElementById('dots1').innerHTML = dots1[s]
-  s = (s + 1) % dots1.length
-}
-function type(text, speed) {
-  if (r < text.length) {
-    document.getElementById('typewriter').innerText += text.charAt(r)
-    r++
-    autoScroll.scrollTop = autoScroll.scrollHeight
-    setTimeout(function () {
-      type(text, speed)
-    }, speed)
-  } else {
-    document.getElementById('dots1').style.display = 'none'
-  }
-}
-function animation1() {
-  r = 0
-  s = 0
-  document.getElementById('typewriter').innerText = ''
-  document.getElementById('dots1').style.display = 'inline'
-  setInterval(updateDots1, 500)
-  setTimeout(function () {
-    type(texts[0], speeds[0])
-  }, 1500)
-}
-function animation2() {
-  r = 0
-  s = 0
-  document.getElementById('typewriter').innerText = ''
-  document.getElementById('dots1').style.display = 'inline'
-  setInterval(updateDots1, 500)
-  setTimeout(function () {
-    type(texts[1], speeds[1])
-  }, 4000)
-}
-function animation3() {
-  r = 0
-  s = 0
-  document.getElementById('typewriter').innerText = ''
-  document.getElementById('dots1').style.display = 'inline'
-  setInterval(updateDots1, 500)
-  setTimeout(function () {
-    type(texts[2], speeds[2])
-  }, 2000)
-}
-
-const animations = [animation1, animation2, animation3]
-document
-  .getElementById('starter-button')
-  .addEventListener('click', function () {
-    animations[currentAnimation]()
-    getInput()
-    /*resetInput();*/
-    currentAnimation = (currentAnimation + 1) % animations.length
-    revealFunctions[currentFunction]()
-    previousFunction = currentFunction
-    currentFunction = (currentFunction + 1) % revealFunctions.length
-  })
+console.log(texts)
